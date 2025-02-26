2bda75a0f1c4c7d9e145e6191e27d2d680e34711
JohnLloyd3 <johnlloydracaza88@gmail.com>
2025-02-26 22:38:22 +0800

    first commit

---
+++ src/database/petadoption (1).sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2025 at 03:29 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `petadoption`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `u_id` int(20) NOT NULL,
+  `u_firstname` varchar(50) NOT NULL,
+  `u_lastname` varchar(50) NOT NULL,
+  `u_email` varchar(50) NOT NULL,
+  `u_username` varchar(50) NOT NULL,
+  `u_password` varchar(50) NOT NULL,
+  `u_type` varchar(50) NOT NULL,
+  `u_status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`u_id`, `u_firstname`, `u_lastname`, `u_email`, `u_username`, `u_password`, `u_type`, `u_status`) VALUES
+(1, 'John Lloyd', 'Racaza', 'johnlloydracaza88@gmail.com', 'johnlloyd23', 'johnlloyd1', 'Admin', 'Active'),
+(2, 'Leonelen ', 'Carmen', 'leonelencarmen11@gmail.com', 'leonelen11', 'leonelen12345', 'User', 'Active'),
+(3, 'Jay Lester', 'Racaza', 'lester@gmail.com', 'lester123', '123123123', 'User', 'Pending');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`u_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
